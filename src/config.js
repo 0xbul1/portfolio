5cbc083b1b7c2e566d44dfa528a350dd9a614436
赵旋 <>
2021-09-18 17:40:40 +0800

    fix config

--- src/config.js
+++ src/config.js
@@ -2,41 +2,41 @@
 export const VITE_APP_CONFIG = {
 
   // 站点名
-  SiteName: 'Hi, I\'m zxx',
+  SiteName: 'Hi, I\'m mjj',
   
   // 首页logo名
-  logoName: 'ZXX',
+  logoName: 'MJJ',
 
   // 第一行描述
   line1Desc: 'I don\'t have to, but I have to',
 
   // 第二行描述
-  line2Desc: 'Sorry, I\'m zxx ：）',
+  line2Desc: 'Sorry, I\'m mjj ：）',
 
   // 项目描述
   projectDesc : [
     {
       id: 1,
       subtitle: 'haha',
-      title: 'This project is for zxx',
+      title: 'This project is for mjj',
       img: 'img1',
     },
     {
       id: 2,
       subtitle: 'hoho',
-      title: 'Always believe that something wonderful is about to happen',
+      title: 'something wonderful is about to happen',
       img: 'img2',
     },
     {
       id: 3,
       subtitle: 'hehe',
-      title: 'Best wishes to you',
+      title: 'Best wishes to mjj',
       img: 'img3',
     },
   ],
 
   // Email
-  email: 'zxx@zxx.com',
+  email: 'mjj@mjj.com',
 
   // phone
   phone: '188-8888-8888'
