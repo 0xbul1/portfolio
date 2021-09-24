bd44cb28b7d599e76d9a5cbbfe67e55ce946511b
赵旋 <>
2021-09-24 18:20:06 +0800

    fix

--- src/config.js
+++ src/config.js
@@ -2,42 +2,42 @@
 export const VITE_APP_CONFIG = {
 
   // 站点名
-  SiteName: 'Hi, I\'m mjj',
+  SiteName: 'Hi, I\'m xuanzhao',
   
   // 首页logo名
-  logoName: 'MJJ',
+  logoName: 'XuanZhao',
 
   // 第一行描述
-  line1Desc: 'I don\'t have to, but I have to',
+  line1Desc: 'It\'s my opensource project',
 
   // 第二行描述
-  line2Desc: 'Sorry, I\'m mjj ：）',
+  line2Desc: 'Hi, I\'m xuanzhao ：）',
 
   // 项目描述
   projectDesc : [
     {
       id: 1,
       subtitle: 'haha',
-      title: 'This project is for mjj',
+      title: 'This project is for you',
       img: 'img1',
     },
     {
       id: 2,
       subtitle: 'hoho',
-      title: 'something wonderful is about to happen',
+      title: 'demo project desc',
       img: 'img2',
     },
     {
       id: 3,
       subtitle: 'hehe',
-      title: 'Best wishes to mjj',
+      title: 'Best wishes to you',
       img: 'img3',
     },
   ],
 
   // Email
-  email: 'mjj@mjj.com',
+  email: 'xuanzhao@88.com',
 
   // phone
-  phone: '188-8888-8888'
+  phone: '1xx-xxxx-xxxx'
 };
