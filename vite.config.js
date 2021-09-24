fae0dc73e1c96558244e31d16980bb1ce547b8fb
赵旋 <>
2021-09-24 18:11:15 +0800

    fix base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [vue(), VitePWA({
     registerType: 'autoUpdate',
     manifest: {
